a45d37be21fce3feeb759efad4afda089359d283
Imran Shaba <shabaimrandev@gmail.com>
2025-11-19 12:09:20 +0100

    Update homepage

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -15,7 +15,6 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
@@ -25,11 +24,6 @@ function showSlides() {
         slideIndex = 1
     }
 
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+    slides[slideIndex-1].style.display = "block";
+    // setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
